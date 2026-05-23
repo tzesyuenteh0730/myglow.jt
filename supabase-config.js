c34759116d4ecd03cfe78e113c945d4d6ea9d10c
tzesyuenteh0730 <tzesyuenteh0730@gmail.com>
2026-05-24 00:13:09 +0800

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.MYGLOW_SUPABASE = {
+  url: "https://fejcguskzzhtyceyvkmv.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlamNndXNrenpodHljZXl2a212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NzEzMTcsImV4cCI6MjA5NTA0NzMxN30.ICs1_a1L-_CMeqs8nqJ2FTCdYwJN8aJ3nNZq4fZOgfk"
+};
